45c6c1597d86acf02adedf7d0a3cfd63842d0d7c
Exauc3 <ziakondayala@gmail.com>
2025-11-19 18:43:31 +0100

    Update config.js
    modification pour lecture

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
-const UNSPLASH_ACCESS_KEY: 'gmfTZk-DAOj3OLulckwfbcfyQnR4Hyzg3aXP1kaugaI';
+// config.js
+const API_KEYS = {
+    // C'est cette variable que script.js va lire
+    UNSPLASH_ACCESS_KEY: 'gmfTZk-DAOj3OLulckwfbcfyQnR4Hyzg3aXP1kaugaI' 
+};
