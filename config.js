6316f28b72a897763f037472bc5b5ae972add927
Exauc3 <ziakondayala@gmail.com>
2025-11-19 19:17:07 +0100

    Update config.js
    changement

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
-const UNSPLASH_ACCESS_KEY = 'gmfTZk-DA0j30Lulckwf bcfyQnR4Hyzg3aXP1kaugaI'
+// ======================================================================
+// Fichier : config.js
+// Contient la clé d'accès publique pour l'API Unsplash.
+// ⚠️ IMPORTANT : N'oubliez pas d'ajouter 'config.js' à votre .gitignore.
+// ======================================================================
 
-;
+// Définition de la variable globale que script.js va lire.
+const UNSPLASH_ACCESS_KEY = "gmfTZk-DAOj3OLulckwfbcfyQnR4Hyzg3aXP1kaugaI"; 
